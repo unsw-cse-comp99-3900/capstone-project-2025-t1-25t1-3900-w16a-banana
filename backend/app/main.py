b9e261d7ea383fbf3d75451147ccc8708f07b803
Seokho Yang <z5337452@ad.unsw.edu.au>
2025-03-07 16:24:22 +1100

    Basic Flask server with docker defined

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Basic message testing"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
