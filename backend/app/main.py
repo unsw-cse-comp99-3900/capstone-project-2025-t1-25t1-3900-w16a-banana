4852e1d59b9628dd95fe80a66a580e0491384167
David0023 <yang200023@gmail.com>
2025-03-07 17:56:43 +1100

    flask_restx library usage with sample api provided

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,6 @@
-from flask import Flask, jsonify
+from app import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return jsonify({"message": "Basic message testing"}), 200
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
