944623f090b1370314515712f079d8cbe8b7fe46
Wen123567 <ximilllu2023@163.com>
2025-03-12 13:39:54 +1100

    fix: route for admin to approve or reject the application

--- backend/routes/application.py
+++ backend/routes/application.py
@@ -9,6 +9,9 @@ from models import *
 
 api = Namespace('application', description='Admin works on the new driver and new restaurant application')
 
+# admin approve or reject application:
+# when the new driver or restaurant applies,
+# or when the existing driver or restaurant updates some important information
 
 # the admin obtains all PENDING applications
 # choose between the driver and restaurant
@@ -38,16 +41,20 @@ class PendingApplications(Resource):
         return [application.dict() for application in applications]
 
 
-# the admin approves the application
-@api.route('/approve/<string:application_type>/<int:user_id>')
+# the admin approves or reject the PENDING application
+@api.route('/<string:application_type>/<int:user_id>/<string:action>')
 class ApproveApplication(Resource):
     @api.expect(auth_header)
-    def post(self, application_type, user_id):
-        """Admin approves the application, enter either driver or restaurant, and use the driver_id or restaurant_id"""
+    @api.description('application_type is either driver or restaurant, user_id is the driver_id or restaurant_id, action is either approve or reject')
+    def post(self, application_type, user_id, action):
+        """Admin action on the application"""
 
         # check the application type
         if application_type not in ['driver', 'restaurant']:
             abort(400, 'Invalid application type')
+
+        if action not in ['approve', 'reject']:
+            abort(400, 'Invalid action')
 
         # check the token
         token = auth_header.parse_args()['Authorization']
@@ -64,8 +71,12 @@ class ApproveApplication(Resource):
         if not application:
             abort(400, 'Invalid application')
 
-        # approve the application
-        application.status = RegistrationStatus.APPROVED
-        db.session.commit()
-
-        return {'message': 'Application approved'}
+        # approve or reject
+        if action == 'reject':
+            application.status = RegistrationStatus.REJECTED
+            db.session.commit()
+            return {'message': 'Application rejected'}
+        else:
+            application.status = RegistrationStatus.APPROVED
+            db.session.commit()
+            return {'message': 'Application approved'}
