b6bc5815461051df0addd9b331d6538dda185967
David0023 <yang200023@gmail.com>
2025-03-13 21:36:47 +1100

    Test api added for debugging purpose

---
+++ backend/routes/test.py
@@ -0,0 +1,41 @@
+from flask_restx import Namespace, Resource, fields, reqparse
+from flask import request, abort
+from werkzeug.datastructures import FileStorage
+from models import *
+
+api = Namespace('test', description='APIs for Testing for developers')
+
+
+@api.route('/<string:user_type>')
+class Users(Resource):
+    def get(self, user_type):
+        """Get all existing users"""
+
+        if user_type == "driver":
+            drivers = []
+            for driver in Driver.query.all():
+                drivers.append(driver.dict())
+            return {"Drivers": drivers}, 200
+        
+        elif user_type == "customer":
+            customers = []
+            for customer in Customer.query.all():
+                customers.append(customer.dict())
+            return {"Customers": customers}, 200
+        
+        elif user_type == "admin":
+            admins = []
+            for admin in Admin.query.all():
+                admins.append(admin.dict())
+            return {"Admins": admins}, 200
+        
+        elif user_type == "restaurant":
+            restaurants = []
+            for restaurant in Restaurant.query.all():
+                restaurants.append(restaurant.dict())
+            return {"Restaurants": restaurants}, 200
+        
+        else:
+            return {"message": "Wrong type"}, 400
+
+        
