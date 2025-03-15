76667a816a9f95d15fc8c6b12058f7ec6868515c
David0023 <yang200023@gmail.com>
2025-03-15 16:42:45 +1100

    Error Fixed: no default values, comparison instead of assignment

--- backend/utils/response.py
+++ backend/utils/response.py
@@ -1,9 +1,9 @@
 from typing import Tuple, Dict, Optional
 
 # Generate error message in JSON format. Recommended approach instead of raising HTTPException.
-def res_error(status_code: int, error_message: Optional[str]) -> Tuple[Dict[str, str], int]:
+def res_error(status_code: int, error_message: Optional[str] = None) -> Tuple[Dict[str, str], int]:
     if status_code == 401 and error_message == None:
-        error_message == 'Unauthorised'
+        error_message = 'Unauthorised'
     elif error_message == None:
         error_message = 'Error'
     return {"message": error_message}, status_code
