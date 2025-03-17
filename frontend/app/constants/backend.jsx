e546e14ee655f7b1325b66af3140ce787437d028
Cai <caixianyu8@gmail.com>
2025-03-17 23:58:55 +1100

    feat: frontend call login customer api

---
+++ frontend/app/constants/backend.jsx
@@ -0,0 +1,1 @@
+export default BACKEND = "http://localhost:11000";
--- frontend/app/signup/customer.jsx
+++ frontend/app/signup/customer.jsx
@@ -6,6 +6,9 @@ import useToast from "../../hooks/useToast";
 import useAuth from "../../hooks/useAuth";
 import PersonalInfoForm from "../../components/PersonalInfoForm";
 import AddressForm from "../../components/AddressForm";
+import axios from "axios";
+import { isStrongPassword } from "validator";
+import BACKEND from "../constants/backend";
 
 export default function CustomerSignup() {
   const router = useRouter();
@@ -24,10 +27,47 @@ export default function CustomerSignup() {
     postcode: "",
   });
 
-  const handleSubmit = () => {
-    // TODO: Add submit
-  };
+  const handleSubmit = async () => {
+    // all fields are required
+    for (const key in form) {
+      if (!form[key]) {
+        showToast("Please fill all fields before submission.", "error");
+        return;
+      }
+    }
 
+    // password must be strong, and password and confirmPassword must match
+    if (!isStrongPassword(form.password)) {
+      showToast("Please pick a stronger password.", "error");
+      return;
+    }
+
+    if (form.password !== form.confirmPassword) {
+      showToast("Passwords do not match.", "error");
+      return;
+    }
+
+    // API call
+    const url = `${BACKEND}/customer/register`;
+    
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      }
+    };
+
+    try {
+      const response = await axios.post(url, form, config);
+      showToast("Registration successful!", "success");
+      login(response.data);
+      router.push("/dashboard");
+    } catch (error) {
+      const errorMessage = error.response?.data?.message || "Something went wrong. Please try again.";
+      showToast(errorMessage, "error");
+    }
+  };
+  
   const handleClear = () => {
     setForm({
       username: "",
