812769cc5951f37491bff818694f0d702a49f1c0
Wen123567 <ximilllu2023@163.com>
2025-03-17 16:07:14 +1100

    feat: add toast component

--- frontend/hooks/useToast.jsx
+++ frontend/hooks/useToast.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
-import { toastContext } from "../store/toastContext";
+import { ToastContext } from "../store/toastContext";
 
 const useToast = () => {
-  return useContext(toastContext);
+  return useContext(ToastContext);
 };
 
 export default useToast;
