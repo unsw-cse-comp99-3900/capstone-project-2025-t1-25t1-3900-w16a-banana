be2796a8f3c172fea09950d44815689429512b3a
fpxlwxc11111 <z5490086@ad.unsw.edu.au>
2025-03-18 21:22:03 +1100

    fix: get scroll view into a component

--- frontend/app/driver/(tabs)/profile.jsx
+++ frontend/app/driver/(tabs)/profile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import { View, Text, TouchableOpacity, ActivityIndicator, Image } from "react-native";
 import { Button } from "react-native-paper";
 import { useRouter } from "expo-router";
 import useAuth from "../../../hooks/useAuth";
 import useToast from "../../../hooks/useToast";
 import ProfileAvatar from "../../../components/ProfileAvatar";
 import LogoutButton from "../../../components/LogoutButton";
+import { BACKEND } from "../../../constants/backend";
+import MyScrollView from "../../../components/MyScrollView";
 
 export default function Profile() {
   const router = useRouter();
@@ -23,43 +25,66 @@ export default function Profile() {
   }
 
   return (
-    <View style={{ flex: 1, alignItems: "center", paddingHorizontal: 16, paddingVertical: 25, backgroundColor: "#f9f9f9" }}>
-      {/* Profile Image Section */}
-      <ProfileAvatar userType="driver" />
+    <MyScrollView>
+      <View style={{ flex: 1, alignItems: "center", paddingHorizontal: 16, paddingVertical: 25, backgroundColor: "#f9f9f9" }}>
+        {/* Profile Image Section */}
+        <ProfileAvatar userType="driver" />
 
-      {/* Username, here do not allow edit */}
-      <View style={{ flexDirection: "row", alignItems: "center", marginTop: 10 }}>
-        <Text style={{ fontSize: 22, fontWeight: "bold" }}>{contextProfile.first_name} {contextProfile.last_name}</Text>
+        {/* Username, here do not allow edit */}
+        <View style={{ flexDirection: "row", alignItems: "center", marginTop: 10 }}>
+          <Text style={{ fontSize: 22, fontWeight: "bold" }}>{contextProfile.first_name} {contextProfile.last_name}</Text>
+        </View>
+
+        {/* Registration Status Indicator */}
+        {contextProfile.registration_status === "PENDING" ? (
+          <>
+            <Text style={{ marginTop: 10, color: "red", fontSize: 16, textAlign: "center" }}>
+              Your profile update is under system review. 
+            </Text>
+            <Text style={{ color: "red", fontSize: 16, textAlign: "center" }}>
+              After approval, you can start receiving orders.
+            </Text>
+          </>
+        ) : null}
+
+        {/* Personal Info */}
+        <View style={{ width: "100%", marginTop: 20, paddingVertical: 10, paddingHorizontal: 18, backgroundColor: "#f0f0f0", borderRadius: 10 }}>
+          <Text style={{ fontSize: 18, fontWeight: "bold", paddingBottom: 7 }}>Personal Information</Text>
+          <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>Email:</Text> {contextProfile.email}</Text>
+          <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>Phone:</Text> {contextProfile.phone}</Text>
+          <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>License Number:</Text> {contextProfile.license_number}</Text>
+          <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>Car Plate:</Text> {contextProfile.car_plate}</Text>
+        </View>
+
+        {/* License and Registration Documents */}
+        <View style={{ width: "100%", marginTop: 20, paddingVertical: 10, paddingHorizontal: 18, backgroundColor: "#f0f0f0", borderRadius: 10 }}>
+          <Text style={{ fontSize: 18, fontWeight: "bold", paddingBottom: 7 }}>Documents</Text>
+          <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>License Number:</Text> {contextProfile.license_number}</Text>
+          <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>Car Plate:</Text> {contextProfile.car_plate}</Text>
+          {/* images */}
+          <Text style={{ fontSize: 16, fontWeight: "bold", marginBottom: 5 }}>License Image:</Text>
+          <Image 
+            source={{ uri: `${BACKEND}/${contextProfile.url_license_image}` }}
+            style={{ width: "100%", height: 200, borderRadius: 10, marginBottom: 10 }}
+            resizeMode="contain"
+          />
+
+          <Text style={{ fontSize: 16, fontWeight: "bold", marginBottom: 5 }}>Registration Paper:</Text>
+          <Image 
+            source={{ uri: `${BACKEND}/${contextProfile.url_registration_paper}` }}
+            style={{ width: "100%", height: 200, borderRadius: 10 }}
+            resizeMode="contain"
+          />
+        </View>
+
+        {/* Profile update button */}
+        <View style={{ flexDirection: "row", marginTop: 20 }}>
+          <Button mode="outlined" icon="pencil" onPress={() => router.push("/driver/EditProfile")}>Edit Profile</Button>
+        </View>
+
+        {/* Logout Button */}
+        <LogoutButton />
       </View>
-
-      {/* Registration Status Indicator */}
-      {contextProfile.registration_status === "PENDING" ? (
-        <>
-          <Text style={{ marginTop: 10, color: "red", fontSize: 16, textAlign: "center" }}>
-            Your profile update is under system review. 
-          </Text>
-          <Text style={{ color: "red", fontSize: 16, textAlign: "center" }}>
-            After approval, you can start receiving orders.
-          </Text>
-        </>
-      ) : null}
-
-      {/* Personal Info */}
-      <View style={{ width: "100%", marginTop: 20, paddingVertical: 10, paddingHorizontal: 18, backgroundColor: "#f0f0f0", borderRadius: 10 }}>
-        <Text style={{ fontSize: 18, fontWeight: "bold", paddingBottom: 7 }}>Personal Information</Text>
-        <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>Email:</Text> {contextProfile.email}</Text>
-        <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>Phone:</Text> {contextProfile.phone}</Text>
-        <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>License Number:</Text> {contextProfile.license_number}</Text>
-        <Text style={{ fontSize: 16 }}><Text style={{ fontWeight: "bold" }}>Car Plate:</Text> {contextProfile.car_plate}</Text>
-      </View>
-
-      {/* Profile update button */}
-      <View style={{ flexDirection: "row", marginTop: 20 }}>
-        <Button mode="outlined" icon="pencil" onPress={() => router.push("/driver/EditProfile")}>Edit Profile</Button>
-      </View>
-
-      {/* Logout Button */}
-      <LogoutButton />
-    </View>
+    </MyScrollView>
   );
 }
