3cd97e22379477c647a843cf726a7f5762cf337b
Cai <caixianyu8@gmail.com>
2025-03-18 11:01:43 +1100

    feat: prepare dashboard page

---
+++ frontend/app/customer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Customer() {
+  return (
+    <View>
+      <Text>Customer</Text>
+    </View>
+  );
+}
