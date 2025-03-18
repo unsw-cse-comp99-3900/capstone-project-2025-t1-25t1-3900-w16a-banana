ffaa17ca45f7f4e5eab526fa5659f03088b1aa66
fpxlwxc11111 <z5490086@ad.unsw.edu.au>
2025-03-19 00:18:17 +1100

    feat: add new pages for restaurant

---
+++ frontend/app/restaurant/(tabs)/_layout.jsx
@@ -0,0 +1,28 @@
+import BottomTabs from "../../../components/BottomTabs";
+import useAuth from "../../../hooks/useAuth";
+
+// normal tabs, when the driver's registration_status is not pending
+const tabs = [
+  { name: "index", title: "Home", icon: "home", visible: true },
+  { name: "orders", title: "Orders", icon: "clipboard-list", visible: true },
+  { name: "notifications", title: "Notifications", icon: "bell", visible: true },
+  { name: "profile", title: "Profile", icon: "account", visible: true },
+  // Hidden tabs
+  { name: "EditProfile", title: "Edit Profile", icon: "account-edit", visible: false },
+];
+
+// when the driver application status is pending, use these tabs
+const pendingTabs = [
+  { name: "index", title: "Home", icon: "home", visible: true },
+  { name: "profile", title: "Profile", icon: "account", visible: true },
+  // hidden tabs
+  { name: "orders", title: "Orders", icon: "clipboard-list", visible: false },
+  { name: "notifications", title: "Notifications", icon: "bell", visible: false },
+  { name: "EditProfile", title: "Edit Profile", icon: "account-edit", visible: false },
+]
+
+export default function TabLayout() {
+  const { contextProfile } = useAuth();
+  const tabsToShow = contextProfile?.registration_status === "PENDING" ? pendingTabs : tabs;
+  return <BottomTabs tabs={tabsToShow} />;
+}
