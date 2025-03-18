e99cadab5142b147c8dec66fe5d56f5187f3986b
fpxlwxc11111 <z5490086@ad.unsw.edu.au>
2025-03-19 00:18:57 +1100

    feat: home page

--- frontend/app/restaurant/(tabs)/index.jsx
+++ frontend/app/restaurant/(tabs)/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
+import useAuth from "../../../hooks/useAuth";
 
 export default function Home() {
+  const { contextProfile, isContextLoading } = useAuth();
+  console.log(contextProfile);
+
+  if (isContextLoading || !contextProfile) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 20 }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Welcome, {contextProfile.name }!</Text>
+      
+      {contextProfile.registration_status === "PENDING" ? (
+        <Text style={{ marginTop: 10, color: "red", fontSize: 18, textAlign: "center" }}>
+          Your account is under review by the Admin. Please wait and come back later.
+        </Text>
+      ) : null}
     </View>
-  )
+  );
 }
