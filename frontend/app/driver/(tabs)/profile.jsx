1dfc929e474086865fb3332f17a336d8f36303af
fpxlwxc11111 <z5490086@ad.unsw.edu.au>
2025-03-18 20:51:40 +1100

    fix: take the profile upload section to a single component

--- frontend/app/driver/(tabs)/profile.jsx
+++ frontend/app/driver/(tabs)/profile.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import useAuth from '../../../hooks/useAuth';
 
 export default function Profile() {
+  const { contextProfile } = useAuth();
+  console.log(contextProfile);
+
+
   return (
     <View>
       <Text>Profile</Text>
