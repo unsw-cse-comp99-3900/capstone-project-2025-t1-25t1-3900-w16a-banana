6df608caf9a4b0b6294fff691857860ebe14a67f
David0023 <yang200023@gmail.com>
2025-03-20 23:36:16 +1100

    type annotations / Card number checker function

--- backend/utils/check.py
+++ backend/utils/check.py
@@ -2,26 +2,49 @@
 from db_model import State
 import re
 
-def is_valid_phone(phone):
-    return phone.startswith("04") and len(phone) == 10 and phone.isdigit()
+def is_valid_card_format(s: str) -> bool:
+    """
+    Returns boolean whether given string is valid card format.
+    4 digit followed/not followed by dash(-); forming total 16 digit.
+    1234-5678-9123-4567
+    """
+    pattern = r"^\d{4}(-?\d{4}){3}$"
+    return bool(re.fullmatch(pattern, s))
 
-def is_valid_postcode(postcode):
+
+def is_valid_phone(phone: str) -> bool:
+    """Return boolean about whether given string is valid phone"""
+    return phone.startswith("0")\
+        and len(phone) == 10\
+        and phone[1] in '23478'\
+        and phone.isdigit()
+
+def is_valid_postcode(postcode: str) -> bool:
+    """Return boolean about whether given string is valid postcode"""
     return len(postcode) == 4 and postcode.isdigit()
 
-def is_valid_state(state):
+def is_valid_state(state: str) -> bool:
+    """Return boolean about whether given string is valid state"""
     try:
         State(state)
         return True
     except ValueError:
         return False
 
-def is_valid_license_number(license_number):
+def is_valid_license_number(license_number: str) -> bool:
+    """Return boolean about whether given string is valid license number"""
     return license_number.isdigit()
 
-def is_valid_abn(abn):
+def is_valid_abn(abn: str):
+    """Return boolean about whether given string is valid abn"""
     return abn.isdigit() and len(abn) == 11
 
 def is_password_safe(password: str) -> tuple[bool, str]:
+    """
+    Return boolean and description about whether given string is safe password.
+    Must be at least of length 8.
+    Must contain at least one upper, lower case, number, and special character
+    """
     if len(password) < 8:
         return False, "Password must have at least 8 characters."
     if not re.search(r"[A-Z]", password):
