d2bc444d3f1943a2df309312cdf25938c420a740
David0023 <yang200023@gmail.com>
2025-03-23 10:48:55 +1100

    Changing routes URL and missing array

--- backend/routes/customer_order/models.py
+++ backend/routes/customer_order/models.py
@@ -57,7 +57,7 @@ get_order_res = api.model("Get Order Response", {
 })
 
 get_all_orders_res = api.model("Get All Orders Response", {
-    "customer_orders": fields.List(fields.Nested(get_order_res))
+    "orders": fields.List(fields.Nested(get_order_res))
 })
 
 
