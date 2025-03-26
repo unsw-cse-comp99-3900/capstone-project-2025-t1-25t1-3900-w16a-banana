db64ff555a0a6f5f8af2199ddbeab75a0c961c2f
David0023 <yang200023@gmail.com>
2025-03-26 15:37:01 +1100

    Driver can now accept, pickup and complete orders

--- backend/routes/driver_order/routes.py
+++ backend/routes/driver_order/routes.py
@@ -26,6 +26,34 @@ class AvailableOrders(Resource):
         return {
             'orders': [format_order(order) for order in orders]
         }, 200
+    
+@api.route('/order/accept/<int:order_id>')
+@api.doc(params={
+    'order_id': 'Customer order ID'
+})
+class AcceptOrder(Resource):
+    @api.expect(auth_header)
+    def post(self, order_id: int):
+        """Accept given customer order"""
+        driver = get_driver_by_token(tokenize(request.headers))
+        if not driver:
+            return res_error(401)
+
+        # Get the customer order
+        customer_order = get_customer_order_by_id(order_id)
+        if not customer_order\
+            or customer_order.driver_id:
+            print(customer_order.driver_id)
+            return res_error(404, 'Order Not Found')
+        
+        if customer_order.order_status != OrderStatus.ACCEPTED\
+            and customer_order.order_status != OrderStatus.READY_FOR_PICKUP:
+            return res_error(400, 'Order Cannot Be Accepted')
+        
+        customer_order.driver_id = driver.driver_id
+        db.session.commit()
+
+        return { 'message': 'Order Accepted' }, 200
 
 
 @api.route('/order/pickup/<int:order_id>')
@@ -46,11 +74,14 @@ class PickupOrder(Resource):
             or customer_order.driver_id != driver.driver_id:
             return res_error(404, 'Order Not Found')
         
+        if customer_order.order_status != OrderStatus.READY_FOR_PICKUP:
+            return res_error(400, 'Order Not Ready')
+
         customer_order.order_status = OrderStatus.PICKED_UP
-        customer_order.pickup_time = datetime.now
+        customer_order.pickup_time = datetime.now()
 
         db.session.commit()
-        return
+        return { 'message': 'Order Picked Up' }, 200
 
 @api.route('/order/complete/<int:order_id>')
 @api.doc(params={
@@ -74,7 +105,7 @@ class CompleteOrder(Resource):
             return res_error(400, 'Order Not Picked Up')
         
         customer_order.order_status = OrderStatus.DELIVERED
-        customer_order.delivery_time = datetime.now
+        customer_order.delivery_time = datetime.now()
 
         db.session.commit()
-        return
+        return { 'message': 'Delivery Completed' }, 200
