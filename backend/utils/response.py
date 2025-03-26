14152cf86ab0be86c28d436ac5f631dcd763244e
David0023 <yang200023@gmail.com>
2025-03-26 14:14:46 +1100

    Add further condition

--- backend/utils/response.py
+++ backend/utils/response.py
@@ -2,8 +2,12 @@ from typing import Tuple, Dict, Optional
 
 # Generate error message in JSON format. Recommended approach instead of raising HTTPException.
 def res_error(status_code: int, error_message: Optional[str] = None) -> Tuple[Dict[str, str], int]:
-    if status_code == 401 and error_message == None:
+    if status_code == 404 and error_message == None:
+        error_message = 'Not Found'
+    elif status_code == 401 and error_message == None:
         error_message = 'Unauthorised'
+    elif status_code == 400 and error_message == None:
+        error_message = 'Bad Request'
     elif error_message == None:
         error_message = 'Error'
     return {"message": error_message}, status_code
