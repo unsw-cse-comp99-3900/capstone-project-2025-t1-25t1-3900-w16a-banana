61d2b4c6d683ba56e2b5b9aaba874032fd67a37e
fpxlwxc11111 <z5490086@ad.unsw.edu.au>
2025-04-03 09:54:57 +1100

    feat: web view uses session storage

---
+++ frontend/store/storage.js
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
+
+// A wrapper for storage on both app and web platforms.
+// On the web platform, use the sessionStorage by default, 
+// so different web tabs will not conflict with each other. 
+export default Storage = {
+  async setItem(key, value) {
+    if (Platform.OS === "web") {
+      sessionStorage.setItem(key, value);
+    } else {
+      await AsyncStorage.setItem(key, value);
+    }
+  },
+
+  async getItem(key) {
+    if (Platform.OS === "web") {
+      return sessionStorage.getItem(key);
+    } else {
+      return await AsyncStorage.getItem(key);
+    }
+  },
+
+  async removeItem(key) {
+    if (Platform.OS === "web") {
+      sessionStorage.removeItem(key);
+    } else {
+      await AsyncStorage.removeItem(key);
+    }
+  },
+};
