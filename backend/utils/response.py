7ac42153f44cc7b1be6c5c1c07b564fe6523415e
David0023 <yang200023@gmail.com>
2025-04-10 15:54:36 +1000

    fix wrong conditions

--- backend/utils/response.py
+++ backend/utils/response.py
@@ -12,9 +12,9 @@ def res_error(status_code: int, error_message: Optional[str] = None) -> Tuple[Di
 
     if status_code == 400:
         error_message = '400 Bad Request: ' + error_message
-    elif status_code == 401 and error_message is None:
+    elif status_code == 401:
         error_message = '401 Unauthorised: ' + error_message
-    elif status_code == 404 and error_message is None:
+    elif status_code == 404:
         error_message = '404 Not Found: ' + error_message
     else:
         error_message = 'Unknown Error: ' + error_message
