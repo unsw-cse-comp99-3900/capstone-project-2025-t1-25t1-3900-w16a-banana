686b0d5f7bea3fe73f4eab044750ba5a275efb91
David0023 <yang200023@gmail.com>
2025-04-12 12:00:45 +1000

    Add category format function

--- backend/db_model/menu/category.py
+++ backend/db_model/menu/category.py
@@ -1,6 +1,14 @@
 """Menu Category DB"""
+from typing import TypedDict, Optional
 from db_model.base import BaseModel
+from db_model import Restaurant
 from utils.db import db
+
+class CategoryFormat(TypedDict):
+    """Formatted Catetgory"""
+    id: int
+    name: str
+    restaurant: Restaurant
 
 class MenuCategory(BaseModel):
     """
@@ -17,3 +25,20 @@ class MenuCategory(BaseModel):
         nullable=False
     )
     name = db.Column(db.String(50), nullable=False)
+
+    def format(self) -> Optional[CategoryFormat]:
+        """
+        Format Category to contain following
+        - id: id of category
+        - name: name of category
+        - restaurant: Restaurant Object
+        """
+        try:
+            restaurant: Restaurant = Restaurant.query.filter_by(id=self.restaurant_id).first()
+            return {
+                'id': self.id,
+                'name': self.name,
+                'restaurant': restaurant
+            }
+        except AttributeError:
+            return None
