ead08a89153d6c54c4468c5b4d5692ee644b43cd
Wen123567 <ximilllu2023@163.com>
2025-04-15 15:38:00 +1000

    feat: router push to chat individual page

--- frontend/components/ChatCard.jsx
+++ frontend/components/ChatCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Pressable } from 'react-native';
 import { Text } from 'react-native-paper';
 import { formatDistanceToNow, differenceInDays, format } from 'date-fns';
 import { BACKEND } from '../constants/backend';
 import capitalize from 'capitalize';
+import useAuth from '../hooks/useAuth';
+import { router } from 'expo-router';
 
 export default function ChatCard({ chat }) {
+  const { contextProfile } = useAuth();
+
   // get the latest message from the chat, 
   // the message is ordered by time in ascending order
   const latestMessage = chat.chats[chat.chats.length - 1];
@@ -26,55 +30,60 @@ export default function ChatCard({ chat }) {
   const name = chat.user.role === 'customer' ? chat.user.name
     : chat.user.role === 'driver' ? `${chat.user.first_name} ${chat.user.last_name}`
     : chat.user.name;
+  
+  // on press, go to this url
+  const targetURL = `${contextProfile.role}/view/chat?userType=${chat.user.role}&userId=${chat.user.id}`;
 
   return (
-    <View 
-      style={{
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        backgroundColor: '#fff',
-        padding: 12,
-        borderRadius: 10,
-        width: '100%',
-        shadowColor: '#000',
-        shadowOpacity: 0.05,
-        shadowOffset: { width: 0, height: 1 },
-        shadowRadius: 3,
-        elevation: 1,
-      }}
-    >
-      {/* this will be 3 columns in the same row */}
-      {/* column 1 is the avatar */}
-      <Image
-        source={{ uri: `${BACKEND}/${chat.user.url_profile_image}` }}
+    <Pressable style={{ width: "100%" }} onPress={() => router.push(targetURL)}>
+      <View 
         style={{
-          width: 50,
-          height: 50,
-          borderRadius: 25,
-          backgroundColor: '#eee',
-          marginRight: 12,
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          backgroundColor: '#fff',
+          padding: 12,
+          borderRadius: 10,
+          width: '100%',
+          shadowColor: '#000',
+          shadowOpacity: 0.05,
+          shadowOffset: { width: 0, height: 1 },
+          shadowRadius: 3,
+          elevation: 1,
         }}
-      />
+      >
+        {/* this will be 3 columns in the same row */}
+        {/* column 1 is the avatar */}
+        <Image
+          source={{ uri: `${BACKEND}/${chat.user.url_profile_image}` }}
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: '#eee',
+            marginRight: 12,
+          }}
+        />
 
-      {/* column 2: Name + message */}
-      <View style={{ flexDirection: 'column', flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start' }}>
-        <Text variant="titleMedium">
-          {capitalize.words(name)}
-        </Text>
-        <Text variant="bodyMedium" style={{ color: '#999' }}>
-          {capitalize(chat.user.role)}
-        </Text>
-        <Text variant="bodyMedium" style={{ color: '#999' }} numberOfLines={1}>
-          {latestMessage.message}
-        </Text>
-      </View>
+        {/* column 2: Name + message */}
+        <View style={{ flexDirection: 'column', flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+          <Text variant="titleMedium">
+            {capitalize.words(name)}
+          </Text>
+          <Text variant="bodyMedium" style={{ color: '#999' }}>
+            {capitalize(chat.user.role)}
+          </Text>
+          <Text variant="bodyMedium" style={{ color: '#999' }} numberOfLines={1}>
+            {latestMessage.message}
+          </Text>
+        </View>
 
-      {/* Time ago */}
-      <View style={{ marginLeft: 8, alignItems: 'flex-end' }}>
-        <Text variant="bodySmall" style={{ color: '#999' }}>
-          {capitalize(timeAgo)}
-        </Text>
+        {/* Time ago */}
+        <View style={{ marginLeft: 8, alignItems: 'flex-end' }}>
+          <Text variant="bodySmall" style={{ color: '#999' }}>
+            {capitalize(timeAgo)}
+          </Text>
+        </View>
       </View>
-    </View>
+    </Pressable>
   );
 }
